93eeef7ce69cf270525a8e0bcfa0d943a939ea11
Jean Denis Caron <j@rotat.io>
2025-05-22 15:25:36 -0400

    Fix config file

--- configurations/instances.js
+++ configurations/instances.js
@@ -13,6 +13,9 @@ exports.instances = [
         "setup": {
           "initial": [
             { command: "sudo apt-get -y install redis", target: "new" },
+            { command: "sudo sed -i 's/^replica-read-only yes$/replica-read-only no/g' /etc/redis/redis.conf", target: "new" },
+            { command: "sudo systemctl daemon-reload", target: "new" },
+            { command: "sudo service redis-server restart", target: "new" },
             { command: "redis-cli get test", target: "new" },
           ],
         },
@@ -33,8 +36,8 @@ exports.instances = [
           "initial": [
             { command: "sudo apt-get -y install redis", target: "new" },
             { command: "sudo mv /etc/redis/redis.conf /etc/redis/redis.old.conf", target: "new" },
-            { command: "echo \"bind * -::*\nprotected-mode no\ndbfilename dump.rdb\nrdb-del-sync-files no\ndir /var/lib/redis\" > ~/redis.conf", target: "new" },
-            { command: "sudo mv ~/redis.conf /etc/redis/redis.conf", target: "new" },
+            { command: "sudo sed -i 's/^bind 127.0.0.1 -::1$/bind * -::*/g' /etc/redis/redis.conf", target: "new" },
+            { command: "sudo sed -i 's/^protected-mode yes$/protected-mode no/g' /etc/redis/redis.conf", target: "new" },
             { command: "sudo systemctl daemon-reload", target: "new" },
             { command: "sudo service redis-server restart", target: "new" },
             { command: "redis-cli get test", target: "new" },
