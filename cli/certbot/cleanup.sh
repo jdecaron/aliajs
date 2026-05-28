e8959fae383d4f6dfa4a5eaa916c0db32bf00298
Jean Denis Caron <j@rotat.io>
2026-05-28 12:41:57 -0400

    Fix hardcoded certbot zoneid in cleanup

--- cli/certbot/cleanup.sh
+++ cli/certbot/cleanup.sh
@@ -8,7 +8,7 @@ if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN/RECORD_ID ]; then
 fi
 
 if [ -n "${RECORD_ID}" ]; then
-  curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/84f030b8865f04484b14f533770df715/dns_records/$RECORD_ID" \
+  curl -s -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/dns_records/$RECORD_ID" \
     -H "Authorization: Bearer $CF_API_KEY" \
     -H "Content-Type: application/json"
 fi
