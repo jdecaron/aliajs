e246c10e304a36ae7dc62c95f4dd92eeccd20053
Jean Denis Caron <j@rotat.io>
2026-07-09 11:55:22 -0400

    Upgrade default image to NodeJS 24

--- configurations/images.js
+++ configurations/images.js
@@ -1,3 +1,3 @@
 export const images = [
-  { ImageId: process.env.ALIAJS_DEFAULT_IMAGE_ID, Name: process.env.ALIAJS_DEFAULT_IMAGE_NAME, major: '18' },
+  { ImageId: process.env.ALIAJS_DEFAULT_IMAGE_ID, Name: process.env.ALIAJS_DEFAULT_IMAGE_NAME, major: '24' },
 ]
