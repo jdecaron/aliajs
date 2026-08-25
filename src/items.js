88902b35dcb1625a786287e5007586064dd74df4
JD Caron <j@rotat.io>
2026-08-25 17:37:10 -0400

    Remove ALIAJS_KEY_NAME value from .env

--- src/items.js
+++ src/items.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const log = logger(__filename)
 
-export const not = ['ALIAJS_DEFAULT_CLOUD', 'ALIAJS_DEFAULT_INSTANCE_TYPE', 'ALIAJS_DEFAULT_LOCATION', 'ALIAJS_DEFAULT_S3_ACCESS_KEY_ID', 'ALIAJS_DEFAULT_S3_URL', 'ALIAJS_DEFAULT_TOP_LEVEL_DOMAIN']
+export const not = ['ALIAJS_DEFAULT_CLOUD', 'ALIAJS_DEFAULT_INSTANCE_TYPE', 'ALIAJS_DEFAULT_LOCATION', 'ALIAJS_DEFAULT_S3_ACCESS_KEY_ID', 'ALIAJS_DEFAULT_S3_URL', 'ALIAJS_DEFAULT_TOP_LEVEL_DOMAIN', 'ALIAJS_KEY_NAME']
 export const sauce = []
 
 export const getItem = ({ items, name }) => {
