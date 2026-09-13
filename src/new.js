b26c35063ef8cbecce055701fbc6c12d5f103032
JD Caron <j@rotat.io>
2026-09-13 08:19:51 -0400

    p

--- src/new.js
+++ src/new.js
@@ -97,25 +97,27 @@ setItem({ items: items.operations, name: `${process.env.ALIAJS_DEFAULT_CLOUD}_AP
   process.env.ALIAJS_DEFAULT_S3_ACCESS_KEY_ID = process.env.ALIAJS_DEFAULT_S3_ACCESS_KEY_ID // TODO
   process.env.ALIAJS_DEFAULT_S3_SECRET_ACCESS_KEY = process.env.ALIAJS_DEFAULT_S3_SECRET_ACCESS_KEY // TODO
   const uniqueString = utils.getUniqueString({ characters: 'abcdefghijklmnopqrstuvwxyz0123456789', length: 3 })
-  await cloud.createBucket({ name: `${process.env.APP_NAME}-${uniqueString}-bucket` })
+  process.env.ALIAJS_DEFAULT_S3_URL = await cloud.createBucket({ name: `${process.env.APP_NAME}-${uniqueString}-bucket` })
+  console.log('🪣', process.env.ALIAJS_DEFAULT_S3_URL)
+  // restic -r s3:s3.us-east-1.amazonaws.com/bucket_name init
 }
 
 let key
 {
-  process.env.ALIAJS_KEY_NAME = `${(new Date()).toISOString().slice(0, 10)}-${process.env.APP_NAME}-key.pem`
-  const keyPath = `${os.homedir()}/.ssh/${process.env.ALIAJS_KEY_NAME}`
-  child_process.execSync(`rm -f ~/.ssh/2026-*`)
-  child_process.execSync(`ssh-keygen -t ed25519 -f ${keyPath} -C "${process.env.ALIAJS_KEY_NAME}" -N ""`)
+  // process.env.ALIAJS_KEY_NAME = `${(new Date()).toISOString().slice(0, 10)}-${process.env.APP_NAME}-key.pem`
+  // const keyPath = `${os.homedir()}/.ssh/${process.env.ALIAJS_KEY_NAME}`
+  // child_process.execSync(`rm -f ~/.ssh/2026-*`)
+  // child_process.execSync(`ssh-keygen -t ed25519 -f ${keyPath} -C "${process.env.ALIAJS_KEY_NAME}" -N ""`)
 
-  key = utils.deepFreeze({
-    name: process.env.ALIAJS_KEY_NAME,
-    value: fs.readFileSync(`${keyPath}.pub`).toString('utf8'),
-  })
-  await cloud.createKey({ key })
-  // process.env.ALIAJS_KEY_NAME = `2026-09-11-aliajs-key.pem` // TODO
+  // key = utils.deepFreeze({
+  //   name: process.env.ALIAJS_KEY_NAME,
+  //   value: fs.readFileSync(`${keyPath}.pub`).toString('utf8'),
+  // })
+  // await cloud.createKey({ key })
+  process.env.ALIAJS_KEY_NAME = `2026-09-11-aliajs-key.pem` // TODO
 }
 
-await newImage()
+// await newImage()
 
 {
   await cloud.upsertDNSZone({ name: process.env.ALIAJS_DEFAULT_TOP_LEVEL_DOMAIN })
@@ -187,6 +189,7 @@ const { domains } = await utils.lazyImport({
 
       await c.ssh.new({ command: `sudo docker exec vaultwarden /vaultwarden backup` })
       const backupFile = (await c.ssh.new({ command: `ls -t /vw-data/ | head -n1` })).replace(/\s$/, '')
+      await c.ssh.new({ command: `export AWS_ACCESS_KEY_ID=${process.env.ALIAJS_DEFAULT_S3_ACCESS_KEY_ID}; export AWS_SECRET_ACCESS_KEY=${process.env.ALIAJS_DEFAULT_S3_SECRET_ACCESS_KEY}; export RESTIC_PASSWORD=${process.env.ALIAJS_VARIABLE_2}; restic -r ${process.env.ALIAJS_DEFAULT_S3_URL}/restic init`, sauce: c.sauce })
       await c.ssh.new({ command: `export AWS_ACCESS_KEY_ID=${process.env.ALIAJS_DEFAULT_S3_ACCESS_KEY_ID}; export AWS_SECRET_ACCESS_KEY=${process.env.ALIAJS_DEFAULT_S3_SECRET_ACCESS_KEY}; export RESTIC_PASSWORD=${process.env.ALIAJS_VARIABLE_2}; restic -r ${process.env.ALIAJS_DEFAULT_S3_URL}/restic backup --stdin --stdin-filename sauce-production-backup --tag sauce-production-backup < /vw-data/${backupFile}`, sauce: c.sauce })
     }},
   ]
